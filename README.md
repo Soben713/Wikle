bcea730cef9ea2c49437fa956e1dce009ea3d11f
Soheil Behnezhad <soheil.behnezhad@gmail.com>
2014-01-17 14:48:08 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+wikle
+=====
+
+A Search Engine on Wikipedia
---
+++ src/main/java/edu/sharif/ce/fall92/mir/pa3/wikle/App.java
@@ -0,0 +1,13 @@
+package edu.sharif.ce.fall92.mir.pa3.wikle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

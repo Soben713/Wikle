68f7679fa3cddc45f283107348bef1283bbb6cb9
MaHsa <mahsa@MyVaio.(none)>
2014-01-19 01:58:05 +0330

    Debug FingerPrint

--- src/main/java/edu/sharif/ce/fall92/mir/pa3/wikle/Main.java
+++ src/main/java/edu/sharif/ce/fall92/mir/pa3/wikle/Main.java
@@ -1,10 +1,14 @@
 package edu.sharif.ce.fall92.mir.pa3.wikle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.sharif.ce.fall92.mir.pa3.wikle.crawler.FingerPrint;
 import edu.sharif.ce.fall92.mir.pa3.wikle.ui.UI;
 
 public class Main {
 	public static void main(String[] args) {
 		new UI();
-//		FingerPrint FP= new FingerPrint();
+
 	}
 }
--- src/main/java/edu/sharif/ce/fall92/mir/pa3/wikle/crawler/FingerPrint.java
+++ src/main/java/edu/sharif/ce/fall92/mir/pa3/wikle/crawler/FingerPrint.java
@@ -3,40 +3,66 @@ package edu.sharif.ce.fall92.mir.pa3.wikle.crawler;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.sharif.ce.fall92.mir.pa3.wikle.utils.Doc;
+
 public class FingerPrint {
-	private static int N=200;
+	private static int N=5;
 	private static double minSim=0.8;
+	private static int codeSize=31;
 	List<List<Integer>> permutations = new ArrayList<List<Integer>>();
 	public FingerPrint(){
 		for(int i=0; i<N; i++){
 			List<Integer> list = new ArrayList<Integer>();
-			for(int j=0; j<64; j++)
+			for(int j=0; j<codeSize; j++)
 				list.add(j);
 			java.util.Collections.shuffle(list);
 			permutations.add(list);
 		}
 	}
-	List<Integer> getFingerPrint(ArrayList<Integer> hashCode){
-		List<Integer> fingerPrint=new ArrayList<Integer>();
+	public ArrayList<Integer>  getFingerPrint(Doc doc){
+		ArrayList<Integer> fingerPrint=new ArrayList<Integer>();
+		String words[]=doc.content.split(" ");
+		List<String> shingles=new ArrayList<String>();
+		for(int i=0; i<words.length-4; i++){
+			String str="";
+			for(int j=0; j<4; j++)
+				str+=words[i+j];
+			shingles.add(str);
+		}
 		for(int i=0; i<N; i++){
-			List<Integer> list=permutations.get(i);
-			int min=100;
-			for(int j=0; j<64; j++){
-				if(hashCode.get(list.get(j))==1 ){
-					Math.min(min, j);
-				}
+			int min=Integer.MAX_VALUE;
+			for(int j=0; j<shingles.size(); j++){
+				int num=shingles.get(j).hashCode();
+				min=Math.min(min, permiutedNum(num, i));
 			}
 			fingerPrint.add(min);
 		}
 		return fingerPrint;
 	}
 	
-	public static boolean calSimilarity(ArrayList<Integer> firstFingerPrint, ArrayList<Integer> secondFingerPrint){
-		int counter=0;
-		for(int i=0; i<N; i++){
-			if(firstFingerPrint.get(i)==secondFingerPrint.get(i))
-				counter++;
+	private int permiutedNum(int num, int l) {
+		String stringNum=Integer.toBinaryString(num);
+		int[] permNum=new int[codeSize];
+		for(int i=0; i<stringNum.length()-1; i++){
+			if(stringNum.charAt(i)=='1'){
+				permNum[permutations.get(l).get(i)]= 1;
+			}
 		}
-		return (double)counter/N>minSim;
+		String ans="";
+		for(int i=0; i<permNum.length; i++)
+			ans+=permNum[i];
+		
+		return Integer.parseInt(ans, 2);
+	}
+	
+	public static boolean calSimilarity(ArrayList<Integer> firstFingerPrint, ArrayList<Integer> secondFingerPrint){
+		double counter=0;
+		for(int i=0; i<N; i++){
+			if(firstFingerPrint.get(i).equals(secondFingerPrint.get(i))){
+				counter++;
+			}
+		}
+		return ((double)counter)/(double)N>minSim;
+		
 	}
 }
